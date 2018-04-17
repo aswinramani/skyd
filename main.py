6307e70d92dc61510468ed4d57d82b6d8c22da0b
aswinramani <aswinramani5@gmail.com>
2018-04-17 08:51:58 +0530

    add changes

--- main.py
+++ main.py
@@ -63,36 +63,72 @@ def detect(img, template, display=False):
         visualize(img)
     return pts
 
+def dev():
+    
+    size = (w, h, channels) = (20, 20, 3)
+    template_image = np.zeros(size, dtype=np.uint8)
+    template_image[4:6, 4:18] = [255, 255, 255]
+    template_image[4:18, 4:6] = [255, 255, 255]
+    # template_image[4:18, 4:6] = [255, 255, 255]
+    t_gray = cv2.cvtColor(template_image, cv2.COLOR_BGR2GRAY)
+    w, h = t_gray.shape[::-1]
+    # visualize(t_gray)
+    img = cv2.imread("images/DSC01798.JPG")
+    image_copy = img.copy()
+    # Step 3 Rescale Image (here we downsample our image)
+    down1 = cv2.pyrDown(image_copy)
+    img_gray = cv2.cvtColor(down1, cv2.COLOR_BGR2GRAY)
+    r_list = [0,45,90,135,180,225,270,315]
+    result = {}
+    for r in r_list:
+        res = cv2.matchTemplate(img_gray,rotate(t_gray, r),cv2.TM_CCORR_NORMED)
+        threshold = np.max(res) - (2 * np.std(res))
+        # threshold = np.max(res) - np.std(res)
+        result[r] = threshold
+        # loc = np.where(res >= threshold)
+        # pts = zip(*loc[::-1])
+        # for pt in pts:
+        #     cv2.rectangle(down1, pt, (pt[0] + w, pt[1] + h), (255,0,0), 1)
+    best_rotation_deg = dict((key,value) for key, value in result.iteritems() if value == max(result.values()))
+    res = cv2.matchTemplate(img_gray,rotate(t_gray, best_rotation_deg.keys()[0]),cv2.TM_CCORR_NORMED)
+    loc = np.where(res >= best_rotation_deg.values()[0])
+    pts = zip(*loc[::-1])
+    for pt in pts:
+        cv2.rectangle(down1, pt, (pt[0] + w, pt[1] + h), (255,0,0), 1)
+    import code
+    code.interact(local=dict(globals(), **locals()))
+    # visualize(down1)
 def run():
-    src_dir = "images/"
-    names = []
-    locations = []
-    count = 0
-    # file_path = os.path.join(directory, file_name)
-    imageList = filter(lambda x: x.split(".")[1] == "JPG", os.listdir(src_dir))
-    for image in imageList:
-        # Step 1 Read source image in bgr 
-        img = cv2.imread(src_dir + image)
-        # Step 2 Add template image for searching L shaped objects 
-        template = get_template_image()
-        # create a copy of the source image 
-        image_copy = img.copy()
-        # Image Pyramids
-        # Step 3 Rescale Image (here we downsample our image)
-        down1 = cv2.pyrDown(image_copy)
-        # Step 4 Detect object using template matching
-        points = map(lambda x: [x[0],x[1]], detect(down1, template))
-        names.append(image)
-        locations.append(points)
+    dev()
+    # src_dir = "images/"
+    # names = []
+    # locations = []
+    # count = 0
+    # # file_path = os.path.join(directory, file_name)
+    # imageList = filter(lambda x: x.split(".")[1] == "JPG", os.listdir(src_dir))
+    # for image in imageList:
+    #     # Step 1 Read source image in bgr 
+    #     img = cv2.imread(src_dir + image)
+    #     # Step 2 Add template image for searching L shaped objects 
+    #     template = get_template_image()
+    #     # create a copy of the source image 
+    #     image_copy = img.copy()
+    #     # Image Pyramids
+    #     # Step 3 Rescale Image (here we downsample our image)
+    #     down1 = cv2.pyrDown(image_copy)
+    #     # Step 4 Detect object using template matching
+    #     points = map(lambda x: [x[0],x[1]], detect(down1, template))
+    #     names.append(image)
+    #     locations.append(points)
 
-    df = pd.DataFrame(data={"Location": locations}, index=names)
-    df.index.name = "FileNames"
-    output_dir = "detections"
-    output_file_name = "positions.csv"
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-    file_path = os.path.join(output_dir, output_file_name)
-    df.to_csv(file_path, encoding='utf-8')
+    # df = pd.DataFrame(data={"Location": locations}, index=names)
+    # df.index.name = "FileNames"
+    # output_dir = "detections"
+    # output_file_name = "positions.csv"
+    # if not os.path.exists(output_dir):
+    #     os.mkdir(output_dir)
+    # file_path = os.path.join(output_dir, output_file_name)
+    # df.to_csv(file_path, encoding='utf-8')
 
 if __name__ == '__main__':
     run()
